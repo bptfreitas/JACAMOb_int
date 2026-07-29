51c1d3b9a5eed14dd1cb62b49358c296ddccc280
Bruno <brunopolicarpo@hotmail.com>
2026-07-29 15:17:33 -0300

    Added initial interrupts code

--- doc/00-JCM/JACAMObProject/run.sh
+++ doc/00-JCM/JACAMObProject/run.sh
@@ -12,6 +12,35 @@ then
 else
     echo "The computer has Maven"
 fi
+
+pkgs=""
+# g++ and binutils
+which g++ 1> /dev/null 2>&1 
+if [[ $? -ne 0 ]]; then
+	echo "Adding g++ to installation"
+	pkgs="$pkgs g++ binutils"
+else
+	echo "'g++' already installed"
+fi
+
+
+# make
+which make 1> /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+	echo "Adding make to installation"
+	pkgs="$pkgs make"
+else
+	echo "'make' already installed"
+fi
+if [[ "$pkgs" != "" ]]; then
+	echo "Installing missing packages"
+	sudo apt install -y $pkgs
+fi
+
+# Compiling interrupts
+echo "Compiling interrupts"
+make -C src/interrupts all
+
 echo "Compiling JACAMOb"
 
 HERE=`pwd`
@@ -21,7 +50,7 @@ cd ../../../
 mvn clean package
 mv target/*-all.jar $HERE/JACAMOb.jar -v
 cd $HERE
-java -jar JACAMOb.jar *.jcm
+LD_LIBRARY_PATH="$HERE/src/interrupts" java -jar JACAMOb.jar *.jcm
 #echo "Starting the MAS!"
 #mvn -f ../../../pom.xml clean compile exec:java \
 #  -Dexec.mainClass="jacamo.infra.JaCaMoLauncher" \
